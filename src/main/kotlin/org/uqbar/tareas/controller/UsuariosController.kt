77c7d3412887b69f0bebab801a9e75bdd14b7f4a
Fernando Dodino <fernando.dodino@gmail.com>
2022-07-07 21:57:49 -0300

    Fix CORS issue

--- src/main/kotlin/org/uqbar/tareas/controller/UsuariosController.kt
+++ src/main/kotlin/org/uqbar/tareas/controller/UsuariosController.kt
@@ -2,12 +2,13 @@ package org.uqbar.tareas.controller
 
 import io.swagger.v3.oas.annotations.Operation
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import org.uqbar.tareas.repository.UsuariosRepository
 
 @RestController
-//@CrossOrigin TODO: Ver si es necesario
+@CrossOrigin
 class UsuariosController {
 
     @Autowired
